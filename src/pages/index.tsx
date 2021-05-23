69520de84d63762a8261b0f035c8baf9f28f8116
jintz <keisuke.igimi@gmail.com>
2021-05-23 12:23:09 +0900

    feat: gatsby-pulugin-graphql-codegenを導入し、TypeScriptでGraphQLのデータを利用できるようにする

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+import {SampleQuery} from "../../graphql-types"
+
+interface Props {
+  data: SampleQuery;
+}
+
+const IndexPage: React.FC<Props> = ({data}) => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
@@ -21,9 +27,20 @@ const IndexPage = () => (
     />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+      <Link to="/blog-post/">{data.allFile.nodes[0].name}</Link>
     </p>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query Sample{
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
